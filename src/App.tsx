58f3cdb74bb87ce22d00f4a4391ea830933c557a
Enzo Vieira <enzovieira16@hotmail.com>
2023-07-09 12:31:42 -0300

    add: Can add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import { TaskItem } from "./components/TaskItem"
+import { useState } from "react"
+import { TaskItem, TaskProps } from "./components/TaskItem"
+
+const arr: TaskProps[] = [
+  {id: 1, titulo: "Task 1", checked: true, date: "7:30 AM, 09/07/2023", status: "complete"},
+  {id: 2, titulo: "Task 2", checked: true, date: "7:30 AM, 09/07/2023", status: "complete"},
+  {id: 3, titulo: "Task 3", checked: false, date: "7:30 AM, 09/07/2023", status: "incomplete"},
+  {id: 4, titulo: "Task 4", checked: false, date: "7:30 AM, 09/07/2023", status: "incomplete"},
+]
 
 function App() {
+  const [taskList, setNewTaskList] = useState(arr)
+  const [selectStatus, setSelectStatus] = useState("all")
+
+  function onDelete(id: number) {
+    const newTaskList = taskList.filter(task => task.id != id)
+    setNewTaskList(newTaskList)
+  }
+
   return (
     <div className="w-full mx-auto max-w-6xl px-4 py-10">
       {/* TITULO */}
@@ -10,23 +26,35 @@ function App() {
       <div className="flex justify-between mt-10">
         <button
           className="bg-primary py-2 px-6 rounded font-medium text-white"
-          onClick={() => console.log("Apertou no botao")}
+          onClick={() => {
+            setNewTaskList([...taskList, {id: Math.random(), titulo: "Nova task", date: "", checked: false, status: "complete"}])
+          }}
         >
           Add Task
         </button>
 
-        <select className="py-2 rounded font-medium w-[150px] border-2 border-black outline-primary">
-          <option>All</option>
-          <option>Complete</option>
-          <option>Incomplete</option>
+        <select onChange={(event) => setSelectStatus(event.target.value)} className="py-2 rounded font-medium w-[150px] border-2 border-black outline-primary">
+          <option value="all">All</option>
+          <option value="complete">Complete</option>
+          <option value="incomplete">Incomplete</option>
         </select>
       </div>
 
       {/* LISTA */}
       <main className="bg-pampas-200 mt-4 p-4 rounded">
-        <TaskItem titulo="Concluir trabalho da faculdade" />
-        <TaskItem titulo="Outro titulo" />
-        <TaskItem titulo="String" />
+        <ul>
+          {taskList.map((task, index) => 
+            task.status == selectStatus ? (
+              <li key={index}>
+                <TaskItem task={task} onDelete={onDelete} />
+              </li>
+            ) : selectStatus == "all" ? (
+              <li key={index}>
+                <TaskItem task={task} onDelete={onDelete} />
+              </li>
+            ) : null
+          )}
+        </ul>
       </main>
     </div>
   )
